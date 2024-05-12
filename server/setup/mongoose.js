1c41bc7a913ebd501e7ec4f1727f24fb60710b02
dRotnes <rotnesdante10@gmail.com>
2024-05-12 13:17:52 +0100

    Initial Setup
    Setup .env file and structured mongoose.js

--- server/setup/mongoose.js
+++ server/setup/mongoose.js
@@ -3,19 +3,18 @@
 */
 'use strict';
 
+// const DATABASE_URL = 'mongodb://127.0.0.1:27017/AirportAI-exercise';
 let mongoose = require('mongoose');
-const DATABASE_URL = 'mongodb://127.0.0.1:27017/AirportAI-exercise';
+const logger = require('console')
+require('dotenv').config();
 
-
-module.exports = (async function() {
-
+const connectDB = async () => {
   const DATABASE_URL = process.env.DATABASE_URI;
 
   if (!DATABASE_URL) {
     throw new Error('DATABASE_URL is not set');
   }
 
-  // Mongoose events.
   mongoose.connection.on('connected', function() {
     logger.info('MongoDB', `Connected to database for env ${process.env.NODE_ENV}`);
   });
@@ -55,4 +54,58 @@ module.exports = (async function() {
     logger.error('MongoDB', 'Unable to connect MongoDB. If problem persists, please restart the server', error);
     return null;
   }
-}());
+};
+
+module.exports = connectDB;
+
+// module.exports = (async function() {
+
+//   const DATABASE_URL = process.env.DATABASE_URI;
+
+//   console.log(DATABASE_URL)
+
+//   if (!DATABASE_URL) {
+//     throw new Error('DATABASE_URL is not set');
+//   }
+
+//   // Mongoose events.
+//   mongoose.connection.on('connected', function() {
+//     logger.info('MongoDB', `Connected to database for env ${process.env.NODE_ENV}`);
+//   });
+//   mongoose.connection.on('open', function() {
+//     logger.info('MongoDB', 'Connection opened');
+//   });
+//   mongoose.connection.on('error', async function(err) {
+//     logger.error('MongoDB', 'Connection error! Throwing error to restart application', err);
+//     // Throw error on mongoose error, so we restart the application.
+//     // eslint-disable-next-line promise/catch-or-return
+//     throw new Error('MongoDB disconnected');
+//   });
+//   mongoose.connection.on('disconnected', function() {
+//     logger.error('MongoDB', 'Disconnected, Reconnecting...');
+//   });
+//   mongoose.connection.on('reconnected', function() {
+//     logger.info('MongoDB', 'Reconnected');
+//   });
+//   mongoose.connection.on('close', function() {
+//     logger.info('MongoDB', 'Closed');
+//   });
+
+//   try {
+//     // Connect to db.
+//     await mongoose.connect(DATABASE_URL, {
+//       connectTimeoutMS: 20000,
+//       serverSelectionTimeoutMS: 5000,
+//       maxPoolSize: 75,
+//     });
+
+//     // Require models after connection.
+//     require('../models');
+//     return mongoose;
+//   }
+//   catch (error) {
+//     // To avoid promise not handled exception.
+//     logger.error('MongoDB', 'Unable to connect MongoDB. If problem persists, please restart the server', error);
+//     return null;
+//   }
+// }());
